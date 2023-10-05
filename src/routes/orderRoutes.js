c556fd75f6315f6a6e21bce3d19c1753038f9f75
Eden Kiefer <edenkieferdev@gmail.com>
2023-10-05 11:52:51 -0300

    feat: add admin validation to routes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import OrderController from '../controllers/OrderController.js'
+import isAdmin from '../middlewares/isAdmin.js'
 
 const router = express.Router()
 
@@ -9,6 +10,6 @@ router
   .get('/orders/filter', OrderController.getFilteredOrders)
   .get('/orders/:id', OrderController.indexOrder)
   .put('/orders/:id', OrderController.updateOrder)
-  .delete('/orders/:id', OrderController.deleteOrder)
+  .delete('/orders/:id', isAdmin(), OrderController.deleteOrder)
 
 export default router
